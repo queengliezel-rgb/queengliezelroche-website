d0c1b6ea620787627936a361d45868e477236bb8
Ritchel Roche <ritchel@example.com>
2025-12-03 09:36:05 +0800

    Add website files

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("btnClick").addEventListener("click", function() {
+    const msg = document.getElementById("message");
+    msg.textContent = "Hello! Your website is working perfectly 🎉";
+});
